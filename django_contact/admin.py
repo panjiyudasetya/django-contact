dc14184820b7552baa82ced95710e8552830185b
panjiyudasetya <panjiyudasetya@gmail.com>
2022-06-02 22:44:49 +0700

    Add contact admin page

--- django_contact/admin.py
+++ django_contact/admin.py
@@ -1,3 +1,64 @@
 from django.contrib import admin
+from django.contrib.admin import (
+    ModelAdmin,
+    TabularInline,
+    RelatedOnlyFieldListFilter
+)
+from django.forms import ModelForm
 
-# Register your models here.
+from .models import (
+    Contact,
+    ContactMembership
+)
+
+
+class ContactMembershipInline(TabularInline):
+    model = ContactMembership
+    fk_name = 'owner'
+    extra = 0
+    fields = (
+        'owner',
+        'contact',
+        'starred',
+    )
+
+
+class ContactForm(ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'user',
+            'nickname',
+            'phone_numbers',
+            'company',
+            'title',
+            'address',
+            'contacts',
+        )
+
+
+class ContactAdmin(ModelAdmin):
+    form = ContactForm
+    inlines = (ContactMembershipInline,)
+    list_display = (
+        'id',
+        'user',
+        'nickname',
+        'company',
+        'title',
+        'address',
+        'created_at',
+        'updated_at',
+    )
+    list_filter = (
+        ('user', RelatedOnlyFieldListFilter),
+    )
+    raw_id_fields = ('user',)
+    search_fields = (
+        'user__first_name',
+        'user__last_name',
+    )
+    list_per_page = 25
+
+
+admin.site.register(Contact, ContactAdmin)
