4dccff2e61c3be87fd4b3dad7f918d2bf4ea94f3
panjiyudasetya <panjiyudasetya@gmail.com>
2022-06-03 22:03:42 +0700

    Add GET contact list endpoint

--- django_contact/urls.py
+++ django_contact/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 
-from django_contact.views import ContactListView
+from django_contact.views import (
+    ContactListView,
+    ContactListDetailView
+)
 
 urlpatterns = [
-    path('contacts/', ContactListView.as_view(), name='contacts'),
+    path('contacts/',
+         ContactListView.as_view(),
+         name='contacts'),
+
+    path('contacts/<int:contact_id>/contacts/',
+         ContactListDetailView.as_view(),
+         name='contacts-of-contact'),
 ]
