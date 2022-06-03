428b43bf4a5879ad98e0b28888faad327f999c1b
panjiyudasetya <panjiyudasetya@gmail.com>
2022-06-03 12:49:19 +0700

    Add all contact view

--- django_contact/views.py
+++ django_contact/views.py
@@ -1,10 +1,13 @@
 from django.http import Http404
+from django.db.models import F
 from rest_framework.generics import ListAPIView
 
 from django_contact.models import Contact
+from django_contact.serializers import ContactSerializer
 
 
 class ContactListView(ListAPIView):
+    serializer_class = ContactSerializer
 
     def get_queryset(self):
         """
@@ -12,8 +15,9 @@ class ContactListView(ListAPIView):
         that belongs to the requester.
         """
         try:
-            contact = Contact.objects.get(user=self.request.user)
+            contact = Contact.objects.first()
         except Contact.DoesNotExist:
             raise Http404
 
-        return contact.contacts.all()
+        return contact.contacts.all()\
+            .annotate(starred=F('contactmembership__starred'))
