8c45dfe7d450c8199e871a66a0d1f73b1a6e7cd8
panjiyudasetya <panjiyudasetya@gmail.com>
2022-06-03 22:03:29 +0700

    Add contact list view

--- django_contact/views.py
+++ django_contact/views.py
@@ -1,13 +1,50 @@
 from django.http import Http404
-from django.db.models import F
-from rest_framework.generics import ListAPIView
+from django.db.models import F, Prefetch
+from rest_framework.generics import (
+    GenericAPIView,
+    ListAPIView
+)
 
-from django_contact.models import Contact
-from django_contact.serializers import ContactSerializer
+from django_contact.models import (
+    Contact,
+    Phone
+)
+from django_contact.serializers import (
+    ContactSerializer,
+    ContactListDetailSerializer
+)
 
 
-class ContactListView(ListAPIView):
+class BaseContactView(GenericAPIView):
+
+    def get_queryset(self):
+        """
+        Return queryset that contains Contact instances
+        with prefetched phone numbers.
+        """
+        prefetch_phone_numbers = Prefetch(
+            'phone_numbers',
+            Phone.objects.all().annotate(is_primary=F('contactphone__is_primary'))
+        )
+        return Contact.objects.all().prefetch_related(prefetch_phone_numbers)\
+            .order_by('id')
+
+
+class ContactListView(BaseContactView, ListAPIView):
+    """
+    Interface:
+    - `GET /contacts/` : List all `Contact` instances.
+    """
     serializer_class = ContactSerializer
+
+
+class ContactListDetailView(BaseContactView, ListAPIView):
+    """
+    Interface:
+    - `GET /contacts/{contact_id}/contacts/` : List all `Contact` instances
+      of the given contact ID.
+    """
+    serializer_class = ContactListDetailSerializer
 
     def get_queryset(self):
         """
@@ -15,9 +52,16 @@ class ContactListView(ListAPIView):
         that belongs to the requester.
         """
         try:
-            contact = Contact.objects.first()
+            contact = super().get_queryset()\
+                .get(id=self.kwargs['contact_id'])
         except Contact.DoesNotExist:
             raise Http404
 
+        # Prefetch contacts' phone numbers that are belongs to the `contact` object
+        prefetch_phone_numbers = Prefetch(
+            'phone_numbers',
+            Phone.objects.all().annotate(is_primary=F('contactphone__is_primary'))
+        )
         return contact.contacts.all()\
+            .prefetch_related(prefetch_phone_numbers)\
             .annotate(starred=F('contactmembership__starred'))
